b582bb93f30d1cc360999438a148ffa3835a45e6
nabighanadeem <nadeemnabigha44@gmail.com>
2021-02-19 22:49:55 +0500

    added lesson-04

--- app/src/main/java/Services/DataService.kt
+++ app/src/main/java/Services/DataService.kt
@@ -12,24 +12,35 @@ object DataService {
     )
 
     val hats = listOf(
-        Product("Nabigha Graphic beanis" , "Rs 500", "hat01"),
-        Product("Nabigha hat black" , "Rs 500", "hat02"),
-        Product("Nabigha hat white" , "Rs 500", "hat03"),
-        Product("Nabigha hat snapback" , "Rs 500", "hat04")
+        Product("Nabigha Graphic beanis" , "Rs 500", "hat1"),
+        Product("Nabigha hat black" , "Rs 500", "hat2"),
+        Product("Nabigha hat white" , "Rs 500", "hat3"),
+        Product("Nabigha hat snapback" , "Rs 500", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Nabigha hoodie gray" , "Rs1000" , "hoodie01"),
-        Product("Nabigha hoodie red" , "Rs1000" , "hoodie02"),
-        Product("Nabigha hoodie blue" , "Rs1000" , "hoodie03"),
-        Product("Nabigha hoodie black" , "Rs1000" , "hoodie04")
+        Product("Nabigha hoodie gray" , "Rs1000" , "hoodie1"),
+        Product("Nabigha hoodie red" , "Rs1000" , "hoodie2"),
+        Product("Nabigha hoodie blue" , "Rs1000" , "hoodie3"),
+        Product("Nabigha hoodie black" , "Rs1000" , "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Nabigha shirt black" , "Rs800" , "shirt01"),
-        Product("Nabigha shirt badge light gray" , "Rs800" , "shirt02"),
-        Product("Nabigha shirt logo red" , "Rs800" , "shirt03"),
-        Product("Nabigha shirt hustle" , "Rs800" , "shirt04"),
-        Product("Nabigha shirt kickflip studio" , "Rs800" , "shirt05")
+        Product("Nabigha shirt black" , "Rs800" , "shirt1"),
+        Product("Nabigha shirt badge light gray" , "Rs800" , "shirt2"),
+        Product("Nabigha shirt logo red" , "Rs800" , "shirt3"),
+        Product("Nabigha shirt hustle" , "Rs800" , "shirt4"),
+        Product("Nabigha shirt kickflip studio" , "Rs800" , "shirt5")
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getproducts(category: String) :List<Product>{
+        when  (category){
+            "SHIRTS" -> return shirts
+            "HOODIES" -> return hoodies
+            "HATS" -> return hats
+            else -> return digitalGood
+        }
+    }
 }
